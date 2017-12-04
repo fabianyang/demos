294f7633200663ec6fe3ef158f27d7dd0fc927a6
yangfan86 <fan107@163.com>
2017-12-04 16:54:49 +0800

    out dsy search file

---
+++ vue-router-demo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div> -->
+  <div id="app">
+    <a href="one.html">one</a><br>
+    <a href="two.html">two</a><br>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  msg: "app"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
